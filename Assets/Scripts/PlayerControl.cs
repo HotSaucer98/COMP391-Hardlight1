8b3e6f2c8f5f2c8252d490fc0fc179cbb72559e4
kcurielj <kcurielj@my.centennialcollege.ca>
2022-03-16 21:48:25 -0400

    make player move and animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,23 +5,93 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
+    public Animator animator;
+    private Rigidbody2D rb2d;
+
+    public float horizontalMov;
+    public float verticalMov;
     public float speed;
-    
+    public float jumpforce;
+    public bool isJumping = false;
+    bool facingRight = true;
+    //private bool m_FacingRight = true;  
+
+    bool isJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horiz, vert;
-        
-        horiz = Input.GetAxis("Horizontal");
-        vert = 0;
-        Vector2 newVelocity = new Vector2(horiz, vert);
-        GetComponent<Rigidbody2D>().velocity = newVelocity * speed;
+
+        horizontalMov = Input.GetAxisRaw("Horizontal") * speed;
+        verticalMov = Input.GetAxisRaw("Vertical") * speed;
+
+        animator.SetFloat("Speed", horizontalMov);
+        animator.SetBool("Jump", false);
+
+        if (verticalMov > 0f)
+        {
+            isJump = true;
+            animator.SetBool("Jump", true);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontalMov > 0 && !facingRight)
+        {
+            Flip();
+        }
+        if (horizontalMov < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        if (horizontalMov > 0.1f || horizontalMov < -0.1f)
+        {
+            rb2d.AddForce(new Vector2(horizontalMov * speed, 0f), ForceMode2D.Impulse);
+        }
+
+        if (verticalMov > 0f && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(0f, jumpforce * verticalMov), ForceMode2D.Impulse);
+        }
+
+    }
+
+    private void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+            animator.SetBool("Jump", false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = true;
+            animator.SetBool("Jump", true);
+        }
     }
 
 }
