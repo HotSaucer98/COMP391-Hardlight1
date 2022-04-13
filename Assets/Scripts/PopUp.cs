8d9da013f7e66a0ab79112d4af41b78b94ab3cc4
daivikshetty666 <97840848+daivikshetty666@users.noreply.github.com>
2022-04-13 13:12:08 +0530

    added prefab

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PopUp : MonoBehaviour
+{
+    public GameObject popUp;
+    public Animator animator;
+    //public TMP_Text popUpText;
+
+    public void Popup(string Text)
+    {
+
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -29,4 +29,5 @@ public class Turret : MonoBehaviour
             interval -= Time.deltaTime;
         }
     }
+    
 }
