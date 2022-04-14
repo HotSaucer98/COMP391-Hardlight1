fc14a5bd9fab89e5171942f9b371776b1db617e1
daivikshetty666 <97840848+daivikshetty666@users.noreply.github.com>
2022-04-14 13:42:41 +0530

    added popup dialogue system

--- Assets/Scripts/PopUp.cs
+++ Assets/Scripts/PopUp.cs
@@ -5,12 +5,38 @@ using UnityEngine;
 
 public class PopUp : MonoBehaviour
 {
-    public GameObject popUp;
-    public Animator animator;
-    //public TMP_Text popUpText;
-
-    public void Popup(string Text)
-    {
-
-    }
+    [SerializeField]
+    Canvas messageCanvas;
+ 
+   void Start()
+   {
+      messageCanvas.enabled = false;
+   }
+ 
+   void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.name == "Robot")
+      {
+        TurnOnMessage();
+      }
+   }
+ 
+   private void TurnOnMessage()
+   {
+      messageCanvas.enabled = true;
+   }
+  
+   void OnTriggerExit2D(Collider2D other)
+   {
+      if (other.name == "Robot")
+      {
+        TurnOffMessage();
+      }
+   }
+ 
+   private void TurnOffMessage()
+   {
+      messageCanvas.enabled = false;
+      //Destroy(this.gameObject, 5.0f);
+   }
 }
